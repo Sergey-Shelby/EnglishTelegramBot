4d93a7effbe049f1ffc4c930527ee9fb13a48eac
Sergey <max.clansy@gmail.com>
2021-05-31 23:24:18 +0300

    Add Telegram

--- EnglishTelegramBot/Startup.cs
+++ EnglishTelegramBot/Startup.cs
@@ -7,11 +7,14 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+using Telegram.Bot.Args;
 
 namespace EnglishTelegramBot
 {
 	public class Startup
 	{
+		private TelegramBotClient _client;
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
@@ -35,6 +38,15 @@ namespace EnglishTelegramBot
 					await context.Response.WriteAsync("Hello Telgram Bot!");
 				});
 			});
+
+			_client = new TelegramBotClient("1874538705:AAEBcO4PJL_MPUHOS1tkF9XPDee7WbMC7nc");
+			_client.StartReceiving();
+			_client.OnMessage += OnMessageHandler;
+		}
+		private async void OnMessageHandler(object sender, MessageEventArgs e)
+		{
+			var msg = e.Message;
+			await _client.SendTextMessageAsync(msg.Chat.Id, $"¬аше сообщение Ч {msg.Text}");
 		}
 	}
 }
