709a9faefb94f7c8c71eb505a12a80627b42e528
Levkovskiy Alex <levkalex98@outlook.com>
2021-06-10 23:34:36 +0300

    fix

--- EnglishTelegramBot/Commands/TrainingWord/CheckSelectWordCommand.cs
+++ EnglishTelegramBot/Commands/TrainingWord/CheckSelectWordCommand.cs
@@ -1,6 +1,10 @@
 ﻿using EnglishTelegramBot.DomainCore.Abstractions;
 using EnglishTelegramBot.DomainCore.Entities;
 using EnglishTelegramBot.DomainCore.Enums;
+using EnglishTelegramBot.DomainCore.Framework;
+using EnglishTelegramBot.DomainCore.Models.WordTrainings;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Telegraf.Net;
 using Telegraf.Net.Abstractions;
@@ -16,12 +20,14 @@ namespace EnglishTelegramBot.Commands.TrainingWord
 
     public class CheckSelectWordCommand : BaseCommand
     {
-        IStatusProvider _statusProvider;
-        IUnitOfWork _unitOfWork;
-        public CheckSelectWordCommand(IStatusProvider statusProvider, IUnitOfWork unitOfWork)
+        private IStatusProvider _statusProvider;
+        private IUnitOfWork _unitOfWork;
+        private IDispatcher _dispatcher;
+        public CheckSelectWordCommand(IStatusProvider statusProvider, IUnitOfWork unitOfWork, IDispatcher dispatcher)
         {
             _statusProvider = statusProvider;
             _unitOfWork = unitOfWork;
+            _dispatcher = dispatcher;
         }
 
         public override async Task ExecuteAsync(TelegrafContext context, UpdateDelegate next)
@@ -29,21 +35,21 @@ namespace EnglishTelegramBot.Commands.TrainingWord
             var status =_statusProvider.GetStatus<Word>(context.User.Id);
             if (status?.Details != null)
             {
-                var user = await _unitOfWork.UserRepository.FetchByTelegramId(context.User.Id);
-               
-                var wordTraining = await NextWordSelectTypeCommand.GetCurrentWordTrainingAsync(_unitOfWork, user);
+                var currentWordTrainings = await _dispatcher.Dispatch<List<WordTraining>>(new FetchCurrentWordTrainingsQuery());
+                var nextWordTraining = currentWordTrainings.OrderBy(x => x.Id).FirstOrDefault(x => x.IsFinished == false);
+
                 var isWrongAnswer = status.Details.EnglishWord.Trim() != context.Update.Message.Text;
                 if (isWrongAnswer)
                 {
-                    await UpdateWordTraining(wordTraining, false, false);
+                    await UpdateWordTraining(nextWordTraining, false, false);
                     await context.ReplyAsync("🤯 Не правильно!\nПопробуй ещё!");
                     return;
                 }
 
-                if (wordTraining.RussianSelect == null)
-                    await UpdateWordTraining(wordTraining, true, true);
+                if (nextWordTraining.RussianSelect == null)
+                    await UpdateWordTraining(nextWordTraining, true, true);
                 else
-                    await UpdateWordTraining(wordTraining, false, true);
+                    await UpdateWordTraining(nextWordTraining, false, true);
 
                 await context.ReplyAsync("🎊 Правильно!");
             }
