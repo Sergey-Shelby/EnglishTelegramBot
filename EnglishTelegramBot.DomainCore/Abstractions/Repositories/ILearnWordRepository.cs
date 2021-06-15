8ec06469596a12a9521910298f8c134919598f5a
Sergey <max.clansy@gmail.com>
2021-06-15 20:08:57 +0300

    fix

--- EnglishTelegramBot.DomainCore/Abstractions/Repositories/ILearnWordRepository.cs
+++ EnglishTelegramBot.DomainCore/Abstractions/Repositories/ILearnWordRepository.cs
@@ -6,9 +6,6 @@ namespace EnglishTelegramBot.DomainCore.Abstractions.Repositories
 {
 	public interface ILearnWordRepository : IBaseRepository<LearnWord>
 	{
-		Task<List<LearnWord>> FetchWordPartOfSpeechForRepeat(int userId);
-		Task<List<int>> FetchWordPartOfSpeechNotInRepeat(int userId);
 		Task<LearnWord> FetchByWordPartOfSpeechIdAsync(int id);
-		Task<List<int>> FetchWordPartOfSpeechRepeat(int userId);
 	}
 }
